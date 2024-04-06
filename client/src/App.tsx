8d0305b82762b35f4ccbbd2a24e9d6134d23b800
HasibKhodayar <121403841+HasibKhodayar@users.noreply.github.com>
2024-04-06 02:12:43 -0400

    initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MemberRegistrationForm from './dashBoards/MemberRegistrationForm';
+
+function App() {
+  return (
+    <MemberRegistrationForm/>
+  );
+}
+
+export default App;
