e6cc8308eeb2074132f08e472c32f78562c7fc54
Hasib Khodayar <khodayarhasib@gmail.com>
2024-04-09 03:05:21 -0400

    health metrics endpoint

--- HealthFitnessClubManagement/src/main/java/com/example/HealthFitnessClubManagement/model/HealthMetric.java
+++ HealthFitnessClubManagement/src/main/java/com/example/HealthFitnessClubManagement/model/HealthMetric.java
@@ -1,57 +1,50 @@
-//package com.example.HealthFitnessClubManagement.model;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import java.time.LocalDate;
-//import java.util.Date;
-//
-//
-//@Entity
-//@Table(name = "HealthMetrics")
-//public class HealthMetric {
-//
-//    @Id
-//    @Getter
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "metricID")
-//    private Long id;
-//
-//    @Getter
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "memberID")
-//    private Member member;
-//
-//    @Getter
-//    @Setter
-//    @Column(name = "metric_date", nullable = false)
-//    private LocalDate metricDate;
-//
-//    @Getter
-//    @Setter
-//    @Column(name = "weight")
-//    private Double weight;
-//
-//    @Getter
-//    @Setter
-//    @Column(name = "height")
-//    private Double height;
-//
-//    @Getter
-//    @Setter
-//    @Column(name = "body_fat_perc")
-//    private Double bodyFatPercentage;
-//
-//    @Getter
-//    @Setter
-//    @Column(name = "muscle_mass")
-//    private Double muscleMass;
-//
-//    public void setMember(Member member) {
-//        this.member = member;
-//    }
-//
-//    public Member getMember() {
-//        return member;
-//    }
-//}
+package com.example.HealthFitnessClubManagement.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "HealthMetrics")
+public class HealthMetric {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "metricID")
+    private Long id;
+
+    @Getter
+    @ManyToOne
+    @JoinColumn(name = "memberID", referencedColumnName = "memberID")
+    private Member member;
+
+    @Getter
+    @Setter
+    @Column(name = "metric_date")
+    private Date metricDate;
+
+    @Getter
+    @Setter
+    @Column(name = "weight")
+    private Double weight;
+
+    @Getter
+    @Setter
+    @Column(name = "height")
+    private Double height;
+
+    @Getter
+    @Setter
+    @Column(name = "body_fat_perc")
+    private Double bodyFatPercentage;
+
+    @Getter
+    @Setter
+    @Column(name = "muscle_mass")
+    private Double muscleMass;
+
+}
--- HealthFitnessClubManagement/src/main/java/com/example/HealthFitnessClubManagement/service/MetricsService.java
+++ HealthFitnessClubManagement/src/main/java/com/example/HealthFitnessClubManagement/service/MetricsService.java
@@ -1,77 +1,56 @@
-//package com.example.HealthFitnessClubManagement.service;
-//
-//import com.example.HealthFitnessClubManagement.model.HealthMetric;
-//import com.example.HealthFitnessClubManagement.model.Member;
-//import com.example.HealthFitnessClubManagement.model.FitnessGoals;
-//import com.example.HealthFitnessClubManagement.repository.FitnessGoalsRepository;
-//import com.example.HealthFitnessClubManagement.repository.HealthMetricsRepository;
-//import com.example.HealthFitnessClubManagement.repository.MemberRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Service;
-//
-//import java.sql.Date;
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//@Service
-//public class MetricsService {
-//
-//    @Autowired
-//    private MemberRepository memberRepository;
-//    @Autowired
-//    private HealthMetricsRepository healthMetricsRepository;
-//
-//
-//    public ResponseEntity<String> createHealthMetric(Member member, LocalDate metricDate , Double weight, Double height, Double bodyFat, Double muscleMass) {
-//        try {
-//            HealthMetric metric = new HealthMetric();
-//            metric.setMember(member);
-//            metric.setMetricDate(metricDate);
-//            metric.setWeight(weight);
-//            metric.setHeight(height);
-//            metric.setBodyFatPercentage(bodyFat);
-//            metric.setMuscleMass(muscleMass);
-//
-//            HealthMetric savedMetric = healthMetricsRepository.save(metric);
-//            return ResponseEntity.ok("Health metric successfully created");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during metric creation");
-//        }
-//    }
-//
-//    public List<HealthMetric> findMetric(Long memberId){
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(() -> new IllegalArgumentException("Member not found with ID: " + memberId));
-//
-//        return healthMetricsRepository.findMetricByMember(member);
-//    }
-//
-//    public void updateMetricDate(Long id, Date newMetricDate){
-//        healthMetricsRepository.updateMetricDate(id, newMetricDate);
-//    }
-//
-//    public void updateWeight(Long id, Double newWeight){
-//        healthMetricsRepository.updateWeight(id, newWeight);
-//    }
-//
-//    public void updateHeight(Long id, Double newHeight){
-//        healthMetricsRepository.updateHeight(id, newHeight);
-//    }
-//
-//    public void updateBodyFat(Long id, double newBodyFat){
-//        healthMetricsRepository.updateBodyFatPerc(id, newBodyFat);
-//    }
-//
-//    public void updateMuscleMass(Long id, double newMuscleMass){
-//        healthMetricsRepository.updateMuscleMass(id, newMuscleMass);
-//    }
-//
-//    public HealthMetric mostRecentMetricDate(Long memberId){
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(() -> new IllegalArgumentException("Member not found with ID: " + memberId));
-//        return healthMetricsRepository.findMostRecentMetric(member);
-//    }
-//}
+package com.example.HealthFitnessClubManagement.service;
+
+import com.example.HealthFitnessClubManagement.model.FitnessGoals;
+import com.example.HealthFitnessClubManagement.model.HealthMetric;
+import com.example.HealthFitnessClubManagement.model.Member;
+import com.example.HealthFitnessClubManagement.repository.HealthMetricsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class MetricsService {
+
+    private final HealthMetricsRepository metricsRepository;
+
+    @Autowired
+    public MetricsService(HealthMetricsRepository metricsRepositoryRepository) {
+        this.metricsRepository = metricsRepositoryRepository;
+    }
+
+    // Method to save FitnessGoals
+    public HealthMetric saveHealthMetric(HealthMetric metric) {
+        return metricsRepository.save(metric);
+    }
+
+    public HealthMetric getHealthMetricById(Long id) {
+        return metricsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("FitnessGoals with ID " + id + " not found"));
+    }
+
+    // Method to get all FitnessGoals
+    public List<HealthMetric> getAllHealthMetrics() {
+        return metricsRepository.findAll();
+    }
+
+
+    // Method to delete FitnessGoals by ID
+    public void deleteHealthMetric(Long id) {
+        metricsRepository.deleteById(id);
+    }
+
+    public void updateMetricDetails(Long metricId, String newMetricDate, double newWeight, double newHeight, double newBodyFat, double newMuscleMass){
+        try{
+            metricsRepository.updateMetricDate(metricId, new Date(((new SimpleDateFormat("yyyy-MM-dd")).parse(newMetricDate)).getTime()));
+            metricsRepository.updateWeight(metricId, newWeight);
+            metricsRepository.updateHeight(metricId, newHeight);
+            metricsRepository.updateBodyFatPerc(metricId, newBodyFat);
+            metricsRepository.updateMuscleMass(metricId, newMuscleMass);
+        }catch (Exception e){e.printStackTrace();System.out.println("Sorry could not update health metrics");}
+    }
+
+}
